d089415a4e1e5bd6eda84fdf4a63cc148f50c1e6
MaksLian <maksimkario@gmail.com>
2020-03-03 11:42:18 -0800

    last one1

--- SoundsGood/FoodDetail.swift
+++ SoundsGood/FoodDetail.swift
@@ -10,84 +10,14 @@ import SwiftUI
 struct FoodDetail: View {
     var food: Food
 
+    var tag = ["hey", "heey"]
+    
     var body: some View {
         VStack {
+            
+        Text(tag[0])
+            
 
-            VStack() {
-                
-            Text(food.FoodName)
-                    .font(.title).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 30).background(Color.red).cornerRadius(50)
-                
-                Spacer().frame(height: 40)
-            Text("Tags:").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.gray).cornerRadius(30)
-                
-                Spacer().frame(height: 40)
-            Text(food.Origin).font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-
-                
-                Spacer().frame(height: 10)
-                
-                // It cannot do more than 10 tags in one HStack. so we got two rows of them
-                
-                HStack() {
-                    
-                    
-                    if food.Breakfast == 1 {
-                        Text("Breakfast").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-                    }
-                    if food.Lunch == 1 {
-                        Text("Lunch").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-                    }
-                    if food.Dinner == 1 {
-                        Text("Dinner").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-                    }
-                    if food.Dessert == 1 {
-                        Text("Dessert").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-                    }
-                    if food.Snack == 1 {
-                        Text("Snack").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-                    }
-                    if food.Vegetarian == 1 {
-                        Text("Vegetarian").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-                    }
-                    if food.Doughy == 1 {
-                        Text("Doughy").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-                    }
-                    if food.Chewy == 1 {
-                        Text("Chewy").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-                    }
-                    if food.Crunchy == 1 {
-                        Text("Crunchy").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-                    }
-                    if food.Sweet == 1 {
-                        Text("Sweet").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-                    }
-                }
-                 
-                Spacer().frame(height: 10)
-        
-                HStack() {
-                    
-                    if food.Sour == 1 {
-                        Text("Sour").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-                    }
-                    if food.Savory == 1 {
-                        Text("Savory").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-                    }
-                    if food.Hot == 1 {
-                        Text("Hot").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-                    }
-                    if food.Cold == 1 {
-                        Text("Cold").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-                    }
-                    if food.Room_Temp == 1 {
-                        Text("Room Temperature").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-                    }
-                }
-            }
-            .padding()
-
-            Spacer()
         }
         .navigationBarTitle(Text(food.FoodName), displayMode: .inline)
     }
