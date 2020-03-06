6e90cc26432059c42b4014e4cf7e361834bd2a1d
MaksLian <maksimkario@gmail.com>
2020-03-06 13:29:46 -0800

    fav list demo view 1

--- SoundsGood/FoodDetail.swift
+++ SoundsGood/FoodDetail.swift
@@ -8,24 +8,45 @@ import SwiftUI
 
 struct FoodDetail: View {
     var food: Food
-    var tags = ["hey", "heey"]
+    
+    
+    
+    var tags = ["Breakfast", "Lunch", "Dinner", "Dessert", "Snack", "Vegetarian", "Doughy", "Chewy", "Crunchy", "Sweet", "Sour", "Savory", "Hot", "Cold", "Room_Temp"]
+    
+    
+    var indes = 0
 
     var body: some View {
+        
+        
         ScrollView {
-        VStack (alignment: .leading, spacing: 16) {
+        
+            VStack (alignment: .leading, spacing: 16) {
             
-            food.image.resizable() .scaledToFit().frame(alignment: .top)
+                Spacer().frame(height: 20)
+                // Food image on top of detail page
+            
+                food.image.resizable() .scaledToFit().frame(alignment: .top)
                         
-                Text(food.FoodName).font(.title).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 30).background(Color.red).cornerRadius(50)
+               // food NAME
+            Text(food.FoodName).font(.title).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 30).background(Color.red).cornerRadius(50)
                 
                 
-                Text("Tags:").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.gray).cornerRadius(30)
-                          
+               // food TAGS
+            Text("Tags:").font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.gray).cornerRadius(30)
+                        
+            Text(food.Origin).font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
                 
-                Text(food.Origin).font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
-
-        }
-        .navigationBarTitle(Text(food.FoodName), displayMode: .inline)
+            
+                ForEach(tags, id: \.self) {  tag in
+                    Button(action: {}) {
+                            Text(tag)
+                    }.font(.subheadline).foregroundColor(.white).padding(.all,5).padding([.leading, .trailing], 10).background(Color.green).cornerRadius(30)
+                }
+                
+                // Navigation bar title is not necessary since we have food name in details already
+            }.navigationBarTitle(Text(food.FoodName), displayMode: .inline)
+            
         }
     }
 }
