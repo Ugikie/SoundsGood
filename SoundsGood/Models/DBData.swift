cca938bc25b1f59a3ee7f3fb600e3eaffc7bcc1b
Austin Adam <austinadam42@gmail.com>
2020-03-08 15:37:16 -0700

    Began implementing db

---
+++ SoundsGood/Models/DBData.swift
@@ -0,0 +1,14 @@
+//
+//  DBData.swift
+//  SoundsGood
+//
+//  Created by Austin Adam on 3/8/20.
+//  Copyright © 2020 MAAD Hungry. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+import CoreLocation
+import SQLite3
+
+
