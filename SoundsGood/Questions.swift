8f688cf60625533df35273b98aaf038c855f131d
MaksLian <maksimkario@gmail.com>
2020-03-10 12:12:30 -0700

    Fixed github issues

--- SoundsGood/Questions.swift
+++ SoundsGood/Questions.swift
@@ -11,8 +11,12 @@ import SwiftUI
 struct Questions: View {
     
     // for test and demo. later gonna use database
-    let questions = ["Where do you wanna eat?"]
-    let answers = ["Cafe", "Restaurant ", "Fast Food"]
+    @State private var answers = ["Cafe", "Restaurant ", "Fast Food"]
+    
+    @State private var QUESTIONSTATE:String = "Where do you wanna eat??"
+    
+    @State private var track:Int = 0
+    
     
     var body: some View {
         
@@ -21,7 +25,7 @@ struct Questions: View {
            
                
                // Print question from an array
-                Text(questions[0]).font(.title).fontWeight(.semibold).bold().foregroundColor(.white).padding(.all,10).padding([.leading, .trailing], 10).background(Color.red).cornerRadius(50)
+                Text(QUESTIONSTATE).font(.title).fontWeight(.semibold).bold().foregroundColor(.white).padding(.all,10).padding([.leading, .trailing], 10).background(Color.red).cornerRadius(50)
                 
                 Spacer().frame(height: 100)
                 
@@ -30,7 +34,14 @@ struct Questions: View {
                 ForEach(answers, id: \.self) {  answer in
                     Button(action: {
                         
-                        
+                        self.QUESTIONSTATE = "Next question!"
+                        if (answer == "Answer1"){
+                            
+                        }
+                        self.answers[0] = "Answer1"
+                        self.answers[1] = "Answer2"
+                        self.answers[2] = "Answer3"
+                        //result(QUESTIONSTATE, answer, track)
                         
                     }) {
                         ZStack {
