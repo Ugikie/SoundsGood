d89f2737163cfbdc86125386b225a13faa60eaea
Andrew-Miner <andrew.miner3@gmail.com>
2020-04-27 23:37:49 -0700

    Finished Task Assigned

---
+++ SoundsGood/TagResults.swift
@@ -0,0 +1,21 @@
+//
+//  TagResults.swift
+//  SoundsGood
+//
+//  Created by Andrew Miner on 4/27/20.
+//  Copyright © 2020 MAAD Hungry. All rights reserved.
+//
+
+import SwiftUI
+
+struct TagResults: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TagResults_Previews: PreviewProvider {
+    static var previews: some View {
+        TagResults()
+    }
+}
